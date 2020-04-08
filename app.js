db9a72c2637c7a9a645d907f6a9bf329190148d4
dianmjs <dianamjs69@gmail.com>
2020-04-08 15:10:25 -0500

    express23

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const pug = require("pug");
 
-app.get("/", (req, res) => {
-  res.send(`<% <h1>"Hola" + req.query.name +"!" </h1>%>`);
+/*app.get("/", (req, res) => {
+  res.send("index" + req.query.name + "!");
+});*/
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/index", (req, res) => {
+  res.render(req.query.name);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
